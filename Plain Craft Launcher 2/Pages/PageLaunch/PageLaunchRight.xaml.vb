f7293d31b5d70d4d49f1ac296fb7292f9ce67138
NewbieXvwu <87637612+NewbieXvwu@users.noreply.github.com>
2025-01-19 14:45:23 +0800

    改得优雅一点

--- Plain Craft Launcher 2/Pages/PageLaunch/PageLaunchRight.xaml.vb
+++ Plain Craft Launcher 2/Pages/PageLaunch/PageLaunchRight.xaml.vb
@@ -4,7 +4,11 @@
     Private Sub Init() Handles Me.Loaded
         PanBack.ScrollToHome()
         PanScroll = PanBack '不知道为啥不能在 XAML 设置
-        PanLog.Visibility = If(ModeDebug, Visibility.Visible, Visibility.Collapsed)
+        PanLog.Visibility = Visibility.Collapsed
+        '社区版提示
+        PanHint.Visibility = If(Convert.ToBoolean(ReadReg("UiLauncherCEHint", "True")), Visibility.Visible, Visibility.Collapsed)
+        LabHint1.Text = "社区版包含未在官方主线版本发布的功能，仅用于尝鲜。请不要向官方仓库反馈社区版的问题哦！"
+        LabHint2.Text = $"若要永久隐藏此提示，请自行查阅代码中的相关部分。"
     End Sub
 
     '暂时关闭快照版提示
